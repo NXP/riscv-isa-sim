94a930b0d31b90cb631279b08989d64c550f2576
Christian Herber <christian.herber@oss.nxp.com>
2024-03-28 10:22:25 +0100

    Fixed alignment checking for store instructions

--- riscv/insns/c_fsw.h
+++ riscv/insns/c_fsw.h
@@ -3,9 +3,9 @@ if (xlen == 32) {
   if (p->extension_enabled(EXT_ZCF)) {
     require_fp;
     MMU.store<uint32_t>(RVC_RS1S + insn.rvc_lw_imm(), RVC_FRS2S.v[0]);
-  } else {
+  } else {  // c.sd - Store pair RV32
     require_extension(EXT_ZCA);
-    require(insn.rd() % 2 == 0);
+    require(insn.rvc_rs2() % 2 == 0);
     MMU.store<uint64_t>(RVC_RS1S + insn.rvc_ld_imm(), RVC_RS2S_PAIR);
   }
 } else { // c.sd
--- riscv/insns/c_fswsp.h
+++ riscv/insns/c_fswsp.h
@@ -3,9 +3,9 @@ if (xlen == 32) {
   if (p->extension_enabled(EXT_ZCF)) {
     require_fp;
     MMU.store<uint32_t>(RVC_SP + insn.rvc_swsp_imm(), RVC_FRS2.v[0]);
-  } else {
+  } else { // c.sdsp - Store pair RV32
     require_extension(EXT_ZCA);
-    require(insn.rd() % 2 == 0);
+    require(insn.rs2() % 2 == 0);
     MMU.store<uint64_t>(RVC_SP + insn.rvc_sdsp_imm(), RVC_RS2_PAIR);
   }
 } else { // c.sdsp
