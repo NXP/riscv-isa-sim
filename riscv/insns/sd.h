dba8c93255a3f4024919c309c98100b8b41d848f
Christian Herber <christian.herber@oss.nxp.com>
2024-04-05 09:41:18 +0200

    Adding alignment check to Zilsd sd

--- riscv/insns/sd.h
+++ riscv/insns/sd.h
@@ -1,2 +1,8 @@
-require((xlen == 64) || p->extension_enabled(EXT_ZILSD));
+if (xlen == 32) {// sd - Store pair RV32
+    require_extension(EXT_ZILSD);
+    require(insn.rs2() % 2 == 0);
+} else {
+    require(xlen == 64)
+}
+
 STORE_D;
